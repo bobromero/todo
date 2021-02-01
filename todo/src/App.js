cf9b5747929098dc7190b9c161db633be33c2bea
rromero123102 <rromero21332@columbushs.com>
2021-02-01 11:22:07 -0500

    finished

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,20 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 //Importing Components
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
+
+  //State
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  //Use Effect
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+  //Functions
+  const filterHandler = () => {
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true))
+        break;
+      
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false))
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  }
   return (
     <div className="App">
       <header>
         <h1>Rob's Todo List</h1>
       </header>
-      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+      <Form 
+        inputText={inputText} 
+        todos={todos} 
+        setTodos={setTodos} 
+        setInputText={setInputText} 
+        setStatus={setStatus}
+        
+      />
 
-      <TodoList setTodos={setTodos} todos={todos} />
+      <TodoList setTodos={setTodos} todos={todos} filteredTodos={filteredTodos} />
     </div>
   );
 }
